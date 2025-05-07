367a898221ae57a73c569c2b4ce2d78d5e011e4f
muditjha20 <muditjha1404@gmail.com>
2025-05-07 11:26:53 -0400

    Add project files.

---
+++ Engine/HealingPotion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    public class HealingPotion : Item
+    {
+        public HealingPotion(int id, string name, string namePlural, int amountToHeal) : base(id, name, namePlural)
+        {
+            AmountToHeal = amountToHeal;
+        }
+        public int AmountToHeal { get; set; }
+    }
+}
---
+++ Engine/LivingCreature.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    public class LivingCreature
+    {
+        public int CurrentHitPoints;
+        public int MaximumHitPoints;
+
+        public LivingCreature(int currentHitPoints, int maximumHitPoints)
+        {
+            CurrentHitPoints = currentHitPoints;
+            MaximumHitPoints = maximumHitPoints;
+        }
+    }
+
+}
